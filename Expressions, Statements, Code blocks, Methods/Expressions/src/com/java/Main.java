1ed5b1a4618c80119c1e91a209648d771a8c99d4
Frank Tran <franktranvantu@gmail.com>
2020-07-19 18:30:32 +0700

    Initial commit

---
+++ Expressions, Statements, Code blocks, Methods/Expressions/src/com/java/Main.java
@@ -0,0 +1,12 @@
+package com.java;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int highScore = 50;
+
+        if(highScore == 50) {
+            System.out.println("This is an expression");
+        }
+    }
+}
